d551824284466d7a13063208c77fa4008593110e
Ming Qu <ming.qu0202@gmail.com>
2018-01-10 14:13:20 +0100

    move class to wordcluster

--- utils.py
+++ utils.py
@@ -885,30 +885,5 @@ def phrase_similarity(_phrase_1, _phrase_2, w2v_model):
  	return float(cosine_similarity)
 
 
-class autovivify_list(dict):
-        '''Pickleable class to replicate the functionality of collections.defaultdict'''
-        def __missing__(self, key):
-                value = self[key] = []
-                return value
-
-        def __add__(self, x):
-                '''Override addition for numeric types when self is empty'''
-                if not self and isinstance(x, Number):
-                        return x
-                raise ValueError
-
-        def __sub__(self, x):
-                '''Also provide subtraction method'''
-                if not self and isinstance(x, Number):
-                        return -1 * x
-                raise ValueError
-
-def find_word_clusters(labels_array, cluster_labels):
-        '''Read in the labels array and clusters label and return the set of words in each cluster'''
-        cluster_to_words = autovivify_list()
-        for c, i in enumerate(cluster_labels):
-            cluster_to_words[ i ].append( labels_array[c] )
-        return cluster_to_words
-
 
 
